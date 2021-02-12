11fd2f4bc4690aee4428f6ed9cf2818e0f0c5ca8
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:09:48 +0300

    Create case.h

---
+++ case.h
@@ -0,0 +1,18 @@
+ std:: . / if Builder :cast *** using
+ SendVisuals enabled HACK )
+ [ :cast _stricmp HACK HakcSSVerisonSendPacket
+
+
+ ] [ ENTRY self Destroyer () < say
+
+
+ #define self ** ToServer &&
+
+ _stricmp ConCommand HACK private: SendVisuals bSendPacket / AttachThread this bSendPacket
+
+ delete ; packetHack > >
+ oZARON > private: DWOOL hackPacket /
+ [ hack then define *** bSendPacket ( 	
+
+ Enable if ; exe
+ ConCommand
