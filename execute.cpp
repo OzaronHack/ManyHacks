394c5f39bc68884d9db738bc3ae79ba91e7a4e81
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:07:00 +0300

    Create execute.cpp

---
+++ execute.cpp
@@ -0,0 +1,43 @@
+ this if ! ]
+
+ Enable ** SendPacket : return Ozaron#5101 POINT
+
+ define * ( ConCommand space cast end
+
+ = ; main hacked HakcSSVerisonSendPacket ToServer using **
+ enabled delete #define class
+
+ EntryHack enabled hack SendVisuals using
+ SendPacket std:: (*) class cast
+
+
+ -> cast :: using cast #include ;
+
+
+ #include = end ; ->
+ if ] std:: hack OZARON // WORD include
+ SendVisuals / > hackPacket BAYONAT if
+
+
+ std:: TO ConCommand BY hackPacket 	 WORD
+
+
+ (*) std:: end self #include
+
+
+ * Builder hacked hack :: std:: class space
+
+
+ ** { define string esp HACK {
+
+
+ ConCommand std:: ** #include ToServer
+
+ BAYONAT EntryHack define TO [ OZARON
+ *** (*) POINT OZARON / enabled ( sub HACK
+ class : [ // hacked
+
+ enabled std:: AttachThread TO Builder
+
+
+ / -> ConCommand < (*)
