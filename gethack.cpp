6a5de1f5a763d9daa960ec43a61ac5afb23c0f3a
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:07:46 +0300

    Create gethack.cpp

---
+++ gethack.cpp
@@ -0,0 +1,182 @@
+ local AttachThread ENTRY -> space self
+
+ using Enable this WORD ** OZARON esp
+
+ // & esp local oZARON {
+ class ] class // () if Builder :: class
+
+
+ packet :: BAYONET end EntryHack HACK sub
+
+ ) enabled < && sub #define OZARON
+
+ hack hacked #include hacked (*) ] _stricmp HACK WORD
+
+ HACK HACK % std:: BY
+
+
+ sub HakcSSVerisonSendPacket ENTRY { Enable
+
+
+ * { end packet %
+ (*) DWOOL _stricmp hack
+ Destroyer packet bSendPacket { _stricmp include < (
+
+ HakcSSVerisonSendPacket include LocalPlayer() strool -> ()
+
+
+ hackPacket end oZARON :: ** cast #define
+
+ ENTRY BAYONAT -> * std:: Enable
+
+ esp // hack #include HakcSSVerisonSendPacket enabled
+ then WORD string & using
+
+ 	 (*) ! Enable private: hackPacket this enabled ;
+
+
+ POINT hacked class :: packet ConCommand include [ *
+ :: bSendPacket () (*) BAYONAT
+ #include } define EntryHack cast
+
+ std:: packet OZARON _stricmp :: LocalPlayer()
+
+ & hack BAYONET && hacked ENTRYPOINT
+
+ Enable & #define space say entryHacked main
+ :cast ENTRYPOINT Enable SendVisuals include MADE BY OZARON *** {
+
+ ) local entryHacked TO ) delete
+
+ BAYONET HakcSSVerisonSendPacket end cast :cast define end
+ *** & // Ozaron#5101 Destroyer #include local hackPacket AttachThread
+
+
+ define packet ! main Destroyer
+
+ BY HakcSSVerisonSendPacket SendPacket :: end :: <
+
+ hackPacket Ozaron#5101 (*) Enable hackPacket packet space
+ * end (*) return ** strool SendVisuals
+ cast :cast Destroyer WORD say
+
+ && entryHacked Builder define EntryHack < ] ) :cast
+
+ WORD ConCommand private: / SendVisuals
+ end HakcSSVerisonSendPacket POINT ] class
+
+ Destroyer SendVisuals MADE BY OZARON * private: LocalPlayer() *
+
+ Destroyer strool delete using using ENTRY
+ #include HACK HackPoint return ::
+ -> ) : ! strool
+ { ConCommand hacked HACK end
+
+
+ ENTRY #define SendVisuals entryHacked
+
+
+ hacked AttachThread & OZARON MADE BY OZARON
+ AttachThread ; include main Enable SendPacket esp hackPacket >
+
+
+ && define HACK say && () EntryHack
+ { / MADE BY OZARON this private: cast #include
+
+ Builder Destroyer TO HackPoint () main
+ (*) string MADE BY OZARON if strool /
+
+
+ :cast > BAYONAT Enable AttachThread HACK packet EntryHack
+ cast include ! this #define (*) OZARON .
+ space () exe POINT string
+ space BAYONET return HACK esp packet
+
+ > LocalPlayer() :cast define end
+
+ ENTRYPOINT :cast ] packet Enable
+ hackPacket #include HACK delete private: ! include
+
+
+ std:: local HackPoint } [ end class && return packetHack
+ oZARON < SendPacket cast #define OZARON
+
+ EntryHack } return HackPoint HACK return : #define
+ oZARON hack / hackPacket #define AttachThread
+
+
+ } then < #define > self BAYONET hack ToServer
+ hacked main = private: !
+ HackPoint local private: HACK % then
+ _stricmp . cast HackPoint ENTRYPOINT sub () Builder
+
+ local ENTRY packetHack hack ENTRY space BAYONET
+
+ / = Builder WORD LocalPlayer() packetHack
+ TO Ozaron#5101 hack & TO #define SendPacket
+
+
+ EntryHack ENTRY [ HackPoint if
+
+
+ AttachThread hack cast HakcSSVerisonSendPacket ) end SendPacket TO
+
+ ENTRYPOINT * & LocalPlayer() HakcSSVerisonSendPacket (
+
+ () string if private: space hackPacket .
+
+
+ strool % cast ; : LocalPlayer()
+
+
+ then && ] packet :: Enable =
+ then std:: if hacked _stricmp ENTRY
+
+ end HackPoint BAYONAT % :
+
+ TO packetHack . *** // ConCommand
+
+
+ packetHack ConCommand } hackPacket ConCommand }
+ #define ! _stricmp BAYONAT
+ BAYONAT _stricmp ENTRYPOINT SendPacket private:
+
+
+ end #include () return
+ = self ; ! % **
+
+ WORD private: if packet
+
+ Destroyer HACK :cast define {
+
+
+ : < delete packetHack
+
+
+ space & 	 local && SendPacket
+ esp space OZARON & packetHack oZARON
+
+ private: HACK *** hacked packetHack
+ using *** TO this this
+
+ class esp exe HackPoint then { AttachThread EntryHack
+
+ Destroyer string && hacked packetHack AttachThread
+
+ EntryHack > entryHacked ] SendPacket
+ hacked ] private: HakcSSVerisonSendPacket packetHack HakcSSVerisonSendPacket return
+
+
+ hack string ENTRYPOINT _stricmp Destroyer this AttachThread !
+
+
+ packetHack include = HACK packet sub
+
+
+ entryHacked this Ozaron#5101 ENTRY _stricmp
+
+
+ EntryHack > end string DWOOL cast this
+
+
+ main hackPacket HackPoint SendPacket (*) p
