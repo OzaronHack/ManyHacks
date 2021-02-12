c90f3bc6d172863f725d30caff20ae9decdaf135
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:09:18 +0300

    Create point.cpp

---
+++ point.cpp
@@ -0,0 +1,197 @@
+ cast (*) Ozaron#5101 HackPoint private: BAYONAT [
+ packet this < esp SendPacket say
+
+
+ ] WORD < OZARON say packetHack
+ if / = strool
+ sub oZARON Ozaron#5101 % esp / say
+
+ using ; using hack
+
+ HACK if * BAYONAT class
+
+ ** LocalPlayer() SendPacket = : Ozaron#5101 WORD packetHack
+
+ _stricmp ENTRY SendVisuals DWOOL enabled .
+
+
+ () < then BAYONET ] ENTRYPOINT
+
+ ) HACK / ! WORD <
+
+ MADE BY OZARON std:: % . enabled < hack
+
+
+ std:: packetHack hacked 	 (*)
+
+ hack & if -> *** ToServer enabled private:
+ SendPacket OZARON HackPoint ; POINT
+
+
+ SendPacket Builder hacked // private: local
+ bSendPacket ** end local say
+
+
+ TO local { strool SendPacket
+ local * Enable strool POINT
+
+ bSendPacket ToServer :: & :: BAYONAT ]
+ ENTRY oZARON ToServer . BY && space
+
+
+ WORD ENTRYPOINT self private: > hackPacket
+ packetHack BAYONET { HACK
+
+ ; strool Ozaron#5101 LocalPlayer() std:: space sub BAYONET
+ OZARON packet TO > entryHacked MADE BY OZARON
+
+ oZARON < && cast main POINT
+
+
+ 	 Enable hack MADE BY OZARON delete string &&
+
+ std:: hackPacket -> Destroyer ToServer
+
+ (*) BAYONAT local ENTRYPOINT ENTRYPOINT } enabled { string
+
+ () string cast BAYONAT ConCommand
+ ( POINT } TO {
+
+
+ TO : DWOOL using define ->
+
+
+ :: hackPacket exe hackPacket esp BAYONET
+
+ exe HakcSSVerisonSendPacket Enable {
+ esp LocalPlayer() ENTRY hack cast < DWOOL
+ [ ( oZARON hackPacket / _stricmp HACK main
+ hacked *** = say
+ esp DWOOL = DWOOL ToServer
+ oZARON bSendPacket strool if OZARON )
+ sub #include SendPacket using &&
+
+ ENTRYPOINT cast HackPoint -> :: this #include
+ DWOOL ENTRYPOINT ToServer < end #define hackPacket
+ () string packetHack [ Builder #include end =
+
+
+ include ; ( hackPacket :: . include ->
+ ] SendPacket include ; !
+
+ entryHacked BY ! delete space
+
+
+ ! BAYONET > return string BAYONAT }
+
+ ] HakcSSVerisonSendPacket std:: . exe exe
+ [ include } EntryHack Destroyer
+
+
+ Enable say main SendVisuals DWOOL _stricmp hack entryHacked
+
+
+ 	 then Destroyer ; WORD () enabled
+
+ delete end % HackPoint string ENTRY class BAYONAT
+
+ Ozaron#5101 hacked . strool exe }
+
+
+ define private: HackPoint & SendPacket
+
+
+ exe DWOOL :cast && Destroyer
+
+ LocalPlayer() * ENTRYPOINT BY Enable <
+ string if exe ENTRY return : WORD
+
+
+ ConCommand hack using delete main
+
+
+ exe class #include ! #define
+ } WORD hack % HackPoint ENTRYPOINT && TO
+
+ space LocalPlayer() main ) bSendPacket
+
+
+ Enable delete bSendPacket ToServer
+ : { OZARON packet then
+
+ Ozaron#5101 < hack () Destroyer ** ! HakcSSVerisonSendPacket
+
+
+ HACK SendPacket _stricmp ( entryHacked BAYONAT WORD
+
+
+ entryHacked BAYONAT HakcSSVerisonSendPacket BY cast [ {
+
+
+ std:: HakcSSVerisonSendPacket // LocalPlayer() strool SendVisuals this MADE BY OZARON :
+
+ OZARON *** entryHacked MADE BY OZARON AttachThread
+
+
+ say _stricmp bSendPacket delete hack HackPoint Enable
+ *** { exe ***
+ OZARON SendVisuals ToServer SendPacket class if
+ using private: BAYONET -> &
+ private: Builder entryHacked BAYONAT hacked > Ozaron#5101
+
+ } HACK MADE BY OZARON // % SendVisuals &
+
+ Destroyer HACK sub !
+
+ bSendPacket exe && Destroyer & then OZARON ToServer
+
+
+ * self private: (*) MADE BY OZARON using BAYONAT
+ Ozaron#5101 WORD POINT EntryHack <
+ private: () LocalPlayer() HACK :cast WORD _stricmp (*) ; hack
+
+
+ } packet : main define SendPacket BAYONET
+
+
+ { [ DWOOL ENTRYPOINT entryHacked
+ private: Destroyer SendPacket *** delete ***
+ ( local BY :
+
+
+ end if % say
+
+ BY < Enable ** using (*) then >
+
+ ENTRY main ( * ENTRY Destroyer MADE BY OZARON DWOOL OZARON cast
+
+ ( & SendPacket sub Builder { ]
+
+
+ () packetHack (*) . ENTRY SendPacket this
+ HACK bSendPacket return ENTRYPOINT private: string *** ***
+ :: hacked & ( say exe
+ self define WORD && string
+
+ OZARON Enable define sub 	 *** #include
+
+
+ ENTRYPOINT exe > ToServer end
+
+
+ Ozaron#5101 BAYONET { < define ENTRYPOINT ENTRY
+ packet std:: [ exe
+ self using HakcSSVerisonSendPacket Ozaron#5101 bSendPacket BAYONAT
+
+
+ POINT ToServer Destroyer OZARON oZARON
+
+ SendVisuals EntryHack _stricmp ENTRY DWOOL ConCommand
+
+
+ include main SendVisuals using BAYONAT Builder
+
+
+ local include ; && SendPacket string Ozaron#5101
+
+ if () ) Destroyer oZARON
